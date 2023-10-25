c5efa90d10180fd39b96a8cd2faa29ff30eefd67
DiegodOliveira <ciprianodiegooliveira@gmail.com>
2023-10-25 08:44:38 -0300

    commit from tuesday 24th

---
+++ src/main/java/com/alunoonline/api/model/MatriculaAluno.java
@@ -0,0 +1,29 @@
+package com.alunoonline.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class MatriculaAluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @ManyToOne
+    @JoinColumn(name = "aluno_id")
+    private Aluno aluno;
+
+    @ManyToOne
+    @JoinColumn(name = "disciplina_id")
+    private Disciplina disciplina;
+
+    private Double nota1, nota2;
+
+    private String status;
+}
